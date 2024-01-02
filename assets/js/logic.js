6ff642a83d7b5ba9907d92f3c551e0afd1935c38
Rachel Atkinson <rachelatkinson@Rachels-Laptop.local>
2024-01-02 19:40:34 +0000

    edited savehighscore function to link it to highscore html file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -84,12 +84,15 @@ function endQuiz() {
 
 // Save high score
 function saveHighscore() {
-    const initials = document.getElementById("initials").value;
+    const initials = document.getElementById("initials").value.trim();
     if (initials !== "") {
       const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-      const newScore = { score: time, initials: initials };
+      const newScore = { score: time, initials };
       highscores.push(newScore);
       localStorage.setItem("highscores", JSON.stringify(highscores));
+      window.location.href = 'highscores.html';
+    } else {
+        alert("Please enter your initials!");
     }
   }
 
