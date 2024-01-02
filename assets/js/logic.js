f0ce8fb4419ed77aa6545f115653facd64739628
Rachel Atkinson <rachelatkinson@Rachels-Laptop.local>
2024-01-02 18:55:17 +0000

    added start quiz function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,14 @@
+// Global variables
 let currentQuestionIndex = 0;
 let time = questions.length * 15; // 15 seconds per question
 let timerId;
+
+// Start quiz function
+function startQuiz() {
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
+  
+    timerId = setInterval(clockTick, 1000);
+  
+    displayQuestion();
+  }
